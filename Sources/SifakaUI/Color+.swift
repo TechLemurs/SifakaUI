fd5b3c0b2f23bbf09d034da719dfbc36a9c92b61
Daniel Leuck <dan@ikayzo.com>
2022-08-02 13:02:18 -0500

    Added support for alpha in Color+

--- Sources/SifakaUI/Color+.swift
+++ Sources/SifakaUI/Color+.swift
@@ -13,7 +13,7 @@ public extension Color {
      Create an RGB or RGBA color using the familiar 0-255 params.
      */
     static func rgb(_ r: Double, _ g: Double, _ b: Double, alpha: Double = 255) -> Color {
-        return Color(red: r / 255, green: g / 255, blue: b / 255)
+        return Color(red: r / 255, green: g / 255, blue: b / 255, opacity: alpha / 255)
     }
 }
 
